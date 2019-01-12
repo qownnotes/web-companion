f7b4f13009d15278a56fbfa428423586ea6f9981
Patrizio Bekerle <patrizio@bekerle.com>
2019-01-12 06:09:25 +0100

    switching to directory

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Builds the extensions for Chrome and Firefox
 ###
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
 # workaround for wrong 20xx version number for the Chrome extension
 sed -i -e 's/"version": "/"version": "20/g' package.json
 npm run build chrome
