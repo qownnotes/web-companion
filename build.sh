b3c25499c1ff0a3e3c0aadca24db7ab1022fc397
Patrizio Bekerle <patrizio@bekerle.com>
2020-08-01 10:22:39 +0200

    Add some "exit 1"

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 ###
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd "$DIR" || exit 1
 
 # workaround for wrong 20xx version number for the Chrome extension
-sed -i -e 's/"version": "/"version": "20/g' package.json
-npm run build chrome
+sed -i -e 's/"version": "/"version": "20/g' package.json || exit 1
+npm run build chrome || exit 1
 
 # switch back to the correct version number format again
-sed -i -e 's/"version": "20/"version": "/g' package.json
-npm run build firefox
+sed -i -e 's/"version": "20/"version": "/g' package.json || exit 1
+npm run build firefox || exit 1
