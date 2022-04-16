57cb80c87d40b215cd6c5691ba21ce6ea7283bc4
Patrizio Bekerle <patrizio@bekerle.com>
2022-04-16 18:44:33 +0200

    Disable obsolete text replacement (#41)

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ cd "$DIR" || exit 1
 # workaround for wrong 20xx version number for the Chrome extension
 sed -i -e 's/"version": "/"version": "20/g' package.json || exit 1
 npm run build chrome || exit 1
-# remove background.scripts from manifest.json for manifest v3 for Chrome
-jq 'del(.background.scripts)' dist/chrome/manifest.json > /tmp/manifest.json && mv /tmp/manifest.json dist/chrome/manifest.json
-# create new package
-pushd dist/chrome && zip -r ../../packages/chrome.zip . && popd
+
+## remove background.scripts from manifest.json for manifest v3 for Chrome
+#jq 'del(.background.scripts)' dist/chrome/manifest.json > /tmp/manifest.json && mv /tmp/manifest.json dist/chrome/manifest.json
+## create new package
+#pushd dist/chrome && zip -r ../../packages/chrome.zip . && popd
 
 # switch back to the correct version number format again
 sed -i -e 's/"version": "20/"version": "/g' package.json || exit 1
