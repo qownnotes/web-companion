fe1d0f94cc68286ea31354047c08d401362a1600
Patrizio Bekerle <patrizio@bekerle.com>
2025-03-31 22:00:22 +0200

    treefmt: improve git pre-commit hook assignment
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -12,7 +12,13 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    # Symlink the pre-commit hook into the .git/hooks directory
-    ln -sf ../../scripts/pre-commit.sh .git/hooks/pre-commit
+    # Determine the repository root
+    REPO_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
+
+    # Check if we are in the repository root
+    if [ "$REPO_ROOT" = "$(pwd)" ]; then
+      # Symlink the pre-commit hook into the .git/hooks directory
+      ln -sf ../../scripts/pre-commit.sh .git/hooks/pre-commit
+    fi
   '';
 }
