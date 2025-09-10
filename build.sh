3300dc59faab237f3a23d99e7777092d311ef307
Patrizio Bekerle <patrizio@bekerle.com>
2025-09-10 22:23:08 +0200

    #86 quasar: fix package paths
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ cd "$DIR" || exit 1
 
 # Build the Chrome extension
 npm run build-chrome || exit 1
-mv dist/bex/Packaged.qownnotes-web-companion.zip dist/package-chrome.zip || exit 1
+mv dist/bex-chrome/Packaged.qownnotes-web-companion.zip dist/package-chrome.zip || exit 1
 
 # Build the Firefox extension
 npm run build-firefox || exit 1
-mv dist/bex/Packaged.qownnotes-web-companion.zip dist/package-firefox.zip || exit 1
+mv dist/bex-firefox/Packaged.qownnotes-web-companion.zip dist/package-firefox.zip || exit 1
 
 # Download sourcecode for Mozilla store
 curl https://github.com/qownnotes/web-companion/archive/refs/heads/main.zip -Lo dist/sourcecode.zip
